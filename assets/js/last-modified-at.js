7acd2053f2569f9262c43f7464b8260430d391bd
Pig30nidaE <Pig30nidaE@gmail.com>
2024-04-25 05:06:42 +0900

    fix: to test last modified at#2

--- assets/js/last-modified-at.js
+++ assets/js/last-modified-at.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function () {
-    if (document.getElementById("last-modified")) {
+    if (document.getElementById("last-modified-at")) {
       fetch("https://api.github.com/repos/moeun2/moeun2.github.io/commits?path=_posts/2022-05-30-jekyll-last-modified-at.md")
         .then((response) => {
           return response.json();
@@ -14,7 +14,7 @@ window.addEventListener("load", function () {
           var year = date[0];
 
           var month = console.log(date[0]);
-          document.getElementById("last-modified").textContent = "last_modified_at : " + modified;
+          document.getElementById("last-modified-at").textContent = "last_modified_at : " + modified;
         });
     }
 });
