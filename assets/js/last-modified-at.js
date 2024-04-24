6361d68559337786c43902bd7cf14822012c7959
Pig30nidaE <Pig30nidaE@gmail.com>
2024-04-25 05:51:20 +0900

    fix: to test last modified at#4

--- assets/js/last-modified-at.js
+++ assets/js/last-modified-at.js
@@ -5,16 +5,20 @@ window.addEventListener("load", function () {
           return response.json();
         })
         .then((commits) => {
-          var modified = commits[0]["commit"]["committer"]["date"].slice(0, 20);
+          var modified = commits[0]["commit"]["committer"]["date"].slice(0, 17);
 
           return modified;
         })
         .then((modified) => {
           var date = modified.split(" ");
-          var year = date[0];
+          var year = date[2];
+
+		  console.log(date);
+		  console.log(year);
 
           var month = console.log(date[0]);
           document.getElementById("last-modified-at").textContent = "last_modified_at : " + modified;
+		  console.log(modified);
         });
     }
 });
